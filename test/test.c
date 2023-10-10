d1f9f17717dfab972c4e175e2772734414d64ce4
donghanyuan <dhy_120@163.com>
2023-10-10 11:19:06 +0800

    update testfile

--- test/test.c
+++ test/test.c
@@ -1,9 +1,25 @@
 #include "libsysy.h"
 
 int main() {
+    // Int IO
     int a = getint(), b = getint();
+    putint(a); putch(32); putint(b); putch(10);
     putint(a + b);
-    putch(10);
+    // Float IO
+    float f1 = getfloat(), f2 = getfloat();
+    putfloat(f1); putch(32); putfloat(f2); putch(10);
+    putfloat(f1 + f2);
+    // Int Array IO
+    int arr[16] = {};
+    int narr = getarray(arr);
+    putint(narr); putch(32); putint(arr[3]); putch(10);
+    putarray(narr, arr);
+    // Float Array IO
+    float farr[16] = {};
+    int nfarr = getfarray(farr);
+    putint(nfarr); putch(32); putfloat(farr[2]); putch(10);
+    putfarray(nfarr, farr);
+    // Timing
     int i = 0;
     starttime();
     while (i < a) {
@@ -14,9 +30,5 @@ int main() {
         i = i + 1;
     }
     stoptime();
-    
-    float f1 = getfloat(), f2 = getfloat();
-    putfloat(f1 + f2);
-    putch(10);
     return 0;
 }
